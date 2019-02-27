7e55243ff6bfa334df3cdf37b4b3334a5a2df79d
Barbara Skrela <barbara.skrela@gmail.com>
2019-02-27 18:35:26 -0500

    Hook up backend to Firestore database

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,13 @@
 const admin = require('firebase-admin');
-// const serviceAccount = require('../../firebase-credentials.json');
+const serviceAccount = require('../../firebase-credentials.json');
 
 // initialize firebase store
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://company-cert.firebaseio.com'
+});
 
-// const db = admin.firestore();
+const db = admin.firestore();
 
 // import the db from any file to access firebase!
-// module.exports = db;
+module.exports = db;
--- src/routes/companies/companies.controller.js
+++ src/routes/companies/companies.controller.js
@@ -1,4 +1,4 @@
-// const db = require('../../db');
+const db = require('../../db');
 
 // const companiesController = (req, res) => {
 //   db.collection('companies')
@@ -18,12 +18,34 @@
 
 // exports.index = get all the companies
 exports.index = (req, res) => {
-  return res.json({ data: 'NOT IMPLEMENTED: Listing of companies' });
+  // return res.json({ data: 'NOT IMPLEMENTED: Listing of companies' });
+  db.collection('companies')
+    .get()
+    .then(snapshot => snapshot.docs.map(doc => doc.data()))
+    .then(data => {
+      res.json({
+        data
+      });
+    })
+    .catch(error => console.error('companies data error', error));
 };
 
 // exports.store = create a new company
 exports.store = (req, res) => {
-  return res.json({ data: 'NOT IMPLEMENTED: Create a new company' });
+  // return res.json({ data: 'NOT IMPLEMENTED: Create a new company' });
+  db.collection('companies')
+    .add({
+      name: 'Brain Box',
+      interview_date: '2019-02-28',
+      // created_at: db.FieldValue.serverTimestamp()
+      created_at: new Date().getTime()
+    })
+    .then(data => {
+      res.json({
+        id: data.id
+      });
+    })
+    .catch(error => console.error('Error adding document: ', error));
 };
 
 // exports.show = get one company
