30d0494a85ab4ef3bee4d78c1e11e8b90e0cf914
Barbara Skrela <barbara.skrela@gmail.com>
2019-02-27 17:23:53 -0500

    Add index and store methods on companies controller

--- src/routes/companies/companies.controller.js
+++ src/routes/companies/companies.controller.js
@@ -1,17 +1,31 @@
-const db = require("../../db");
+// const db = require('../../db');
 
-const companiesController = (req, res) => {
-    db.collection('companies')
-    .get()
-    .then(snapshot => snapshot.docs.map(doc => doc.data()))
-    .then(data=>{
-        res.json({
-            data
-        })
-    })
-    .catch (error => console.error('companies data error', error));
+// const companiesController = (req, res) => {
+//   db.collection('companies')
+//     .get()
+//     .then(snapshot => snapshot.docs.map(doc => doc.data()))
+//     .then(data => {
+//       res.json({
+//         data
+//       });
+//     })
+//     .catch(error => console.error('companies data error', error));
+// };
+
+// module.exports = {
+//   companiesController
+// };
+
+// exports.index = get all the companies
+exports.index = (req, res) => {
+  return res.json({ data: 'NOT IMPLEMENTED: Listing of companies' });
 };
 
-module.exports = {
-    companiesController
-}
+// exports.store = create a new company
+exports.store = (req, res) => {
+  return res.json({ data: 'NOT IMPLEMENTED: Create a new company' });
+};
+
+// exports.show = get one company
+// exports.update = update a company
+// exports.destroy = delete a company
--- src/routes/companies/companies.router.js
+++ src/routes/companies/companies.router.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
-const { companiesController } = require("./companies.controller");
+const companiesController = require('./companies.controller');
 
 const router = express.Router();
 
-router.get("", companiesController);
-router.get("/rangle", (req,res)=>{
-    return res.json({bridge: "now!"})
-});
+// router.get('', companiesController);
+
+router.get('/', companiesController.index);
+router.post('/', companiesController.store);
+
+// router.get('/rangle', (req, res) => {
+//   return res.json({ bridge: 'now!' });
+// });
 
 module.exports = {
-    companiesRouter: router
-}
+  companiesRouter: router
+};
