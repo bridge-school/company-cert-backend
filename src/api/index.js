95757cd8009ff1fdbc6541651c9a7ba7b9e78e2d
Kristyna Svecova <kristyna@rewardops.com>
2019-02-27 08:12:23 -0500

    Setup eslint and prettier

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 
-const { healthRouter } = require('../routes/health/health.router')
+const { healthRouter } = require('../routes/health/health.router');
 
 const router = express.Router();
-router.use("/health", healthRouter);
+router.use('/health', healthRouter);
 
 module.exports = router;
