fcdd58c458bb7df401526cc73d180628593eef87
cocoral <hxth2018@gmail.com>
2019-03-03 20:24:41 -0500

    Added /frontend-data end point:
    displaying all the front end data for the company form to be renederd

---
+++ src/routes/frontend-data/frontend-data.controller.js
@@ -0,0 +1,19 @@
+/* 
+*  This is the end point for fetching all the
+*  Front end data  
+*/
+
+const db = require('../../db');
+
+// exports.index = get all the front end data for render
+exports.index = (req, res) => {
+  db.collection('front_end_data')
+    .get()
+    .then(snapshot => snapshot.docs.map(doc => doc.data()))
+    .then(data => {
+      res.json({
+        data
+      });
+    })
+    .catch(error => console.error('frontend-data collection error', error));
+};
