ed2cfd59e8178cb146dd6d58b32dbf286c7b2e69
Kristyna Svecova <kristyna@rewardops.com>
2019-03-06 17:15:30 -0500

    Add test for companies

---
+++ src/routes/companies/companies.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');	
+
+ const { companiesRouter } = require('./companies.router');	
+
+ describe('tests for the companies check endpoint', () => {	
+  it('returns a 200', () => {	
+    expect(true).toBe(true);	
+  });	
+});
