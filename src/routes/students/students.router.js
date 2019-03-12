9ca40ad7ee6169e74a24ef562f7ec23416d15d22
Kristyna Svecova <30747712+Kikisek@users.noreply.github.com>
2019-03-12 19:10:09 -0400

    Merge dev to master (#18)
    * Setup eslint and prettier (#1)
    
    * feature: added endpoint /companies, connected to firebase (#2)
    
    * feature: added endpoint /companies, connected to firebase
    
    * feature: added error handling for /companies endpoint
    
    * Feature/add create company endpoint (#3)
    
    * Add index and store methods on companies controller
    
    * Hook up backend to Firestore database
    
    * Accept request data and save to database
    
    * Make the post request work with company data (#4)
    
    * Make the post request work with company data
    
    * Post also checked items from the checklist
    
    * Feature/front end data (#5)
    
    * Added /frontend-data end point:
    
    displaying checklist,tech and industry for the company form to be rendered
    
    * Formated /frontend-data returned value
    
    * Save selected industry tags and tech tags to database (#7)
    
    * Feature/get single company (#8)
    
    * Remove unused endpoint;
    Create single company endpoint
    
    * Adjust get company call to get actual data
    
    * Add test for companies
    
    * Make the test pass
    
    * Fix response when getting a single company" (#9)
    
    * Return company id from database (#11)
    
    * add students endpoint and index method (#13)
    
    * Resolve the CORS issue on backend (#12)
    
    * add timestamp setting, change date to ISO (#16)
    
    * Feature/add student store method (#17)
    
    Add store method on student enpoint

--- src/routes/students/students.router.js
+++ src/routes/students/students.router.js
@@ -5,6 +5,7 @@ const studentsController = require('./students.controller');
 const router = express.Router();
 
 router.get('/', studentsController.index);
+router.post('/', studentsController.store);
 
 module.exports = {
   studentsRouter: router
